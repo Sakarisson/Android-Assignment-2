e39f1ea7960641463ade2ed16960d3131ca6d02c
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-10 20:26:25 +0300

    Added a bit of button logic

--- app/src/main/java/com/sakarisson/kristian/androidassignment2/MainActivity.java
+++ app/src/main/java/com/sakarisson/kristian/androidassignment2/MainActivity.java
@@ -2,12 +2,23 @@ package com.sakarisson.kristian.androidassignment2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button translateTo;
+    private Button translateFrom;
+    private Button about;
+    private Button quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        translateTo = findViewById(R.id.translateToButton);
+        translateFrom = findViewById(R.id.translateFromButton);
+        about = findViewById(R.id.aboutButton);
+        quit = findViewById(R.id.quitButton);
     }
 }
