7611f595a0c4d7ec7d217d7afe43edf86ad0dadb
Kristian Sakarisson <kristian@sakarisson.com>
2018-04-11 21:04:50 +0300

    Finished translating functions and added functions to exit and bout

--- app/src/main/java/com/sakarisson/kristian/androidassignment2/MainActivity.java
+++ app/src/main/java/com/sakarisson/kristian/androidassignment2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sakarisson.kristian.androidassignment2;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,11 +25,31 @@ public class MainActivity extends AppCompatActivity {
             startActivity(translateToIntent);
         }
     };
-
     OnClickListener translateFromListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
             startActivity(translateFromIntent);
+        }
+    };
+    OnClickListener aboutListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle("About");
+            alertDialog.setMessage("The Robber Language is a Swedish language game. It became popular after the booke about Kalle Blomkvist by Astrid Lindgren, where the children use it as a code, both at play and in solving actual crimes. The principle is easy enough. Every consonant (spelling matters, not pronounciation) is doubled, and an o is inserted in-between. Viwels are left intact. (From Wikipedia)");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+    };
+    OnClickListener quitListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finishAndRemoveTask();
         }
     };
 
@@ -43,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
         translateFromIntent = new Intent(this, TranslateFromActivity.class);
         translateFrom.setOnClickListener(translateFromListener);
         about = findViewById(R.id.aboutButton);
+        about.setOnClickListener(aboutListener);
         quit = findViewById(R.id.quitButton);
+        quit.setOnClickListener(quitListener);
     }
 }
